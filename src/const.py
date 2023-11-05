16859c61cc1435d836770756db2e9c89357878e5
Ardhani Dzaky Ralfiano <82014329+ardhanidzaky@users.noreply.github.com>
2023-11-06 01:15:22 +0700

    Create const.py

---
+++ src/const.py
@@ -0,0 +1,5 @@
+MODEL_CKP = {
+    'effnet': 'src/models/pretrained/efficientnet_b0.pth'
+    , 'shufflenet': 'src/models/pretrained/shufflenetv2.pth'
+    , 'mobilenet': 'src/models/pretrained/mobilenet.pth'
+}
